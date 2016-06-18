b8d2fb234a2e0a2d7529e249ff05d2b4436d70df
michelle_zhang <michelle_zhang@epam.com>
2016-06-18 11:31:12 +0800

    Step4: Modify a widget add widget settings: enable/disable multiline for textbox and save message to history

--- src/mibackoffice/backoffice/src/com/mi/backoffice/widgets/step3/MiChatController.java
+++ src/mibackoffice/backoffice/src/com/mi/backoffice/widgets/step3/MiChatController.java
@@ -3,7 +3,9 @@ package com.mi.backoffice.widgets.step3;
 import com.hybris.cockpitng.annotations.SocketEvent;
 import com.hybris.cockpitng.annotations.ViewEvent;
 import com.hybris.cockpitng.util.DefaultWidgetController;
+import org.zkoss.zhtml.Messagebox;
 import org.zkoss.zk.ui.event.Events;
+import org.zkoss.zk.ui.select.annotation.WireVariable;
 import org.zkoss.zul.Label;
 import org.zkoss.zul.Textbox;
 
@@ -16,12 +18,21 @@ import org.zkoss.zul.Textbox;
  */
 public class MiChatController extends DefaultWidgetController {
 
+    @WireVariable
     private Label lastMsgLabel;
+    @WireVariable
     private Textbox msgInput;
 
     @ViewEvent(componentID = "sendBtn", eventName = Events.ON_CLICK)
     public void sendMsg() {
         sendOutput("outgoingMsg", msgInput.getText());
+
+        //Step4c: Modify a widget
+        // access the setting and trigger storing the message in history
+       if (this.getWidgetSettings().getBoolean("history")) {
+           Messagebox.show("Saved to history"); // assume this is some real logic before this.
+       }
+
     }
 
     @SocketEvent(socketId = "incomingMsg")
